ebf30ac603b09f56ef08c03fd9264fbe80aa00fa
taso0096 <jin2931121@yahoo.co.jp>
2021-04-21 17:47:27 +0900

    [Add] k平均法アセンブリ

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,5 +6,11 @@
 
 <script>
 export default {
+  mounted() {
+    const go = new Go();
+    WebAssembly.instantiateStreaming(fetch('kmeans.wasm'), go.importObject).then(result => {
+      go.run(result.instance);
+    });
+  }
 };
 </script>
