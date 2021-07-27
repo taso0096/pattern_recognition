3295e78543a9c787599a9c2f3b08e1c670df55b9
taso0096 <jin2931121@yahoo.co.jp>
2021-07-27 17:00:05 +0900

    [Update] EMアルゴリズム実装

--- firebase/src/views/EMAlgorithm.vue
+++ firebase/src/views/EMAlgorithm.vue
@@ -123,32 +123,40 @@ export default {
       }
     }
   }),
+  computed: {
+    D: () => 2
+  },
   methods: {
     rnorm() {
       return Math.sqrt(-2*Math.log(1 - Math.random()))*Math.cos(2*Math.PI*Math.random());
     },
     generateData() {
+      const D = this.D;
+      const N = Number(this.nodeCount);
+      const K = Number(this.clusterCount);
       this.X = [];
+      this.starParameters.pi = [];
       this.starParameters.mu = [];
       this.starParameters.sigma = [];
-      [...Array(Number(this.clusterCount))].forEach(() => {
-        const randomCoord = [~~(Math.random()*this.clusterCount*200), ~~(Math.random()*this.clusterCount*200)];
+      [...Array(K)].forEach(() => {
+        const randomCoord = [~~(Math.random()*K*20), ~~(Math.random()*K*20)];
         const X = [];
-        [...Array(Math.ceil(this.nodeCount/this.clusterCount))].forEach(() => {
-          X.push([~~(this.rnorm()*50) + randomCoord[0], ~~(this.rnorm()*50) + randomCoord[1]]);
+        [...Array(Math.ceil(N/K))].forEach(() => {
+          X.push([~~(this.rnorm()*5) + randomCoord[0], ~~(this.rnorm()*5) + randomCoord[1]]);
         });
         this.X.push(...X);
-        this.starParameters.pi.push(1/this.clusterCount);
+        this.starParameters.pi.push(1/K);
         this.starParameters.mu.push(randomCoord);
         const e = math.mean(X, 0);
         const v = math.variance(X, 0, 'uncorrected');
         const cov = X.reduce((cov, x) => cov + (x[0] - e[0])*(x[1] - e[1]), 0)/X.length;
-        const sigma = math.add(math.subtract(math.multiply(math.ones(2, 2), cov), math.diag([...Array(2)].map(() => cov))), math.diag(v));
+        const sigma = math.add(math.subtract(math.multiply(math.ones(D, D), cov), math.diag([...Array(D)].map(() => cov))), math.diag(v));
         this.starParameters.sigma.push(sigma);
       });
       this.setChartdata();
     },
     setChartdata() {
+      const K = Number(this.clusterCount);
       const datasets = [];
       datasets.push({
         label: 0,
@@ -156,7 +164,7 @@ export default {
           x: point[0],
           y: point[1]
         })),
-        backgroundColor: `hsl(${360*0/this.clusterCount}, 100%, 70%)`
+        backgroundColor: `hsl(${360*0/K}, 100%, 70%)`
       });
       this.chartdata = {
         datasets
@@ -164,15 +172,99 @@ export default {
       this.$refs.chart.renderChart(this.chartdata, this.options);
     },
     calcN(x, mu, sigma) {
-      const D = 2;
+      const D = this.D;
       const a = 1/(2*Math.PI)**(D/2);
       const b = 1/math.det(sigma)**(1/2);
       const c = math.multiply(-1/2, math.transpose(math.subtract(x, mu)));
       const d = math.inv(sigma);
       const e = math.subtract(x, mu);
-      return math.multiply(a, b, math.exp(math.multiply(c, d, e)));
+      return a*b*math.exp(math.multiply(c, d, e));
+    },
+    calcLikelihood(X, pi, mu, sigma) {
+      const K = Number(this.clusterCount);
+      let likelihood = 0;
+      X.forEach(x => {
+        [...Array(K).keys()].forEach(k => {
+          likelihood += Math.log(pi[k]*this.calcN(x, mu[k], sigma[k]));
+        });
+      });
+      return likelihood;
+    },
+    calcGamma(X, pi, mu, sigma) {
+      const K = Number(this.clusterCount);
+      const gamma = [];
+      X.forEach(x => {
+        gamma.push([]);
+        let sum = 0;
+        [...Array(K).keys()].forEach(k => {
+          sum += pi[k]*this.calcN(x, mu[k], sigma[k]);
+        });
+        [...Array(K).keys()].forEach(k => {
+          gamma.slice(-1)[0].push(pi[k]*this.calcN(x, mu[k], sigma[k])/sum);
+        });
+      });
+      return gamma;
+    },
+    calcMu(X, gamma, Nm) {
+      const K = Number(this.clusterCount);
+      const mu = [];
+      X.forEach((x, n) => {
+        mu.push([]);
+        [...Array(K).keys()].forEach(m => {
+          mu.slice(-1)[0].push(math.multiply(1/Nm[m], gamma[n][m], x));
+        });
+      });
+      return math.sum(mu, 0);
+    },
+    calcSigma(X, gamma, Nm, mu) {
+      const K = Number(this.clusterCount);
+      const sigma = [];
+      X.forEach((x, n) => {
+        sigma.push([]);
+        [...Array(K).keys()].forEach(m => {
+          sigma.slice(-1)[0].push(math.multiply(1/Nm[m], gamma[n][m], math.multiply(math.transpose([math.subtract(mu[m], x)]), [math.subtract(mu[m], x)])));
+        });
+      });
+      return math.sum(sigma, 0);
     },
     calcEMAlgorithm() {
+      const startTime = Date.now();
+      const D = this.D;
+      const N = Number(this.nodeCount);
+      const K = Number(this.clusterCount);
+      // パラメータ初期値
+      this.emParameters.pi = [...Array(K)].map(() => Math.random()/K);
+      this.emParameters.mu = [...Array(K)].map(() => [...Array(D)].map(() => Math.random()*5));
+      this.emParameters.sigma = [...Array(K)].map(() => math.add(math.multiply(math.ones(D, D), Math.random()*5), math.diag([...Array(D)].map(() => Math.random()*5))));
+
+      let count = 0;
+      let delta = 1;
+      while (count < 200 && Math.abs(delta) >= 0.01) {
+        // E-step
+        const gamma = this.calcGamma(this.X, ...Object.values(this.emParameters));
+        const Nm = math.sum(gamma, 0);
+
+        // M-step
+        const newParameters = {
+          pi: math.divide(Nm, N),
+          mu: this.calcMu(this.X, gamma, Nm),
+          sigma: this.calcSigma(this.X, gamma, Nm, this.emParameters.mu)
+        };
+
+        // 対数尤度関数
+        const l = this.calcLikelihood(this.X, ...Object.values(this.emParameters));
+        const newL = this.calcLikelihood(this.X, ...Object.values(newParameters));
+        this.emParameters = { ...newParameters };
+        delta = newL - l;
+        count++;
+      }
+      this.calcTime = Date.now() - startTime;
+
+      // 結果
+      console.log(count);
+      console.log(this.starParameters);
+      console.log(this.emParameters);
+      console.log(this.calcLikelihood(this.X, ...Object.values(this.starParameters)), this.calcLikelihood(this.X, ...Object.values(this.emParameters)));
     }
   }
 };
