574f921232bbab36eda251952bc0b39c67d245a6
taso0096 <jin2931121@yahoo.co.jp>
2021-12-23 21:59:29 +0900

    [Update] リファクタリング

--- firebase/src/views/MultilayerPerceptron.vue
+++ firebase/src/views/MultilayerPerceptron.vue
@@ -15,6 +15,13 @@
               type="number"
             />
           </v-col>
+          <v-col>
+            <v-text-field
+              v-model="M"
+              label="中間層ユニット数"
+              type="number"
+            />
+          </v-col>
         </v-row>
         <v-row>
           <v-col>
@@ -22,6 +29,15 @@
               v-model="calcTime"
               readonly
               label="処理時間（ms）"
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-text-field
+              v-model="error"
+              readonly
+              label="二乗誤差"
             />
           </v-col>
         </v-row>
@@ -76,10 +92,12 @@ export default {
   },
   data: () => ({
     nodeCount: 100,
-    calcTime: 0,
+    calcTime: null,
     data: [],
+    M: 5,
     w1: null,
     w2: null,
+    error: null,
     chartdata: {},
     options: {
       legend: {
@@ -107,11 +125,6 @@ export default {
       }
     }
   }),
-  computed: {
-    D: () => 2,
-    M: () => 5,
-    K: () => 1
-  },
   methods: {
     generateData() {
       this.data = [];
@@ -120,6 +133,8 @@ export default {
       });
       this.w1 = null;
       this.w2 = null;
+      this.error = null;
+      this.calcTime = null;
       this.setChartdata();
     },
     setChartdata() {
@@ -153,40 +168,24 @@ export default {
     },
     recognize(x) {
       // 中間層
-      const Z = [];
-      for (let j = 0; j < this.M; ++j) {
-        let a = 0;
-        for (let i = 0; i < this.D; ++i) {
-          a += this.w1[j][i]*x[i];
-        }
-        Z.push(Math.tanh(a));
-      }
-      Z[0] = 1; // バイアス
+      const Z = math.tanh(math.multiply(this.w1, x));
+      Z[0] = 1;
       // 出力層
-      const Y = [];
-      for (let k = 0; k < this.K; ++k) {
-        Y.push(0);
-        for (let j = 0; j < this.M; ++j) {
-          Y[k] += this.w2[k][j]*Z[j];
-        }
-      }
+      const Y = math.multiply(this.w2, Z);
       return { Z, Y };
     },
-    calcError(X, T) {
-      let error = 0;
-      for (let n = 0; n < this.nodeCount; ++n) {
-        const { Y } = this.recognize(X[n]);
-        for (let k = 0; k < this.K; ++k) {
-          error += (Y[k] - T[n][k])*(Y[k] - T[n][k])/2;
-        }
-      }
-      return error;
+    calcError() {
+      const X = this.data.map(p => [1, p[0]]);
+      const T = this.data.map(p => [p[1]]);
+      return X.map((xn, n) => [xn, n])
+        .reduce((eSum, [xn, n]) => eSum + this.recognize(xn).Y.map((yk, k) => [yk, k])
+          .reduce((e, [yk, k]) => e + ((yk - T[n][k])**2)/2, 0), 0);
     },
     calcMultilayerPerceptron() {
       const startTime = Date.now();
       // 多層パーセプトロン
       const D = 2; // 入力ユニット数
-      const M = 5; // 中間層ユニット数
+      const M = Number(this.M); // 中間層ユニット数
       const K = 1; // 出力ユニット数
       const X = this.data.map(p => [1, p[0]]); // [バイアス, x_i]
       const T = this.data.map(p => [p[1]]);
@@ -203,27 +202,17 @@ export default {
         X.forEach((x, n) => {
           const { Z, Y } = this.recognize(x);
           // 出力層の誤差を計算
-          const d2 = [...new Array(K).keys()].reduce((d2, k) => {
-            d2.push(Y[k] - T[n][k]);
-            return d2;
-          }, []);
+          const d2 = math.subtract(Y, T[n]);
           // 隠れ層の誤差を計算
           const d1 = [...new Array(M).keys()].reduce((d1, j) => {
-            d1.push((1 - Z[j]**2)*[...new Array(K).keys()].reduce((tmp, k) => tmp + this.w2[k][j]*d2[k], 0));
+            d1.push((1 - Z[j]**2)*this.w2.map((w2k, k) => [w2k, k]).reduce((tmp, [w2k, k]) => tmp + w2k[j]*d2[k], 0));
             return d1;
           }, []);
-          for (let j = 0; j < M; ++j) {
-            for (let i = 0; i < D; ++i) {
-              this.w1[j][i] -= ETA*d1[j]*x[i];
-            }
-          }
-          for (let k = 0; k < K; ++k) {
-            for (let j = 0; j < M; ++j) {
-              this.w2[k][j] -= ETA*d2[k]*Z[j];
-            }
-          }
+          this.w1 = math.subtract(this.w1, math.multiply(ETA, math.transpose([d1]), [x]));
+          this.w2 = math.subtract(this.w2, [math.multiply(ETA, d2, [Z])]);
         });
       });
+      this.error = this.calcError();
       this.calcTime = Date.now() - startTime;
       this.setChartdata();
     }
